a3351160903b52ac7fbd65176ddfbb3220216eef
wubabka <wubabca@gmail.comm>
2017-09-01 20:30:44 +0300

    Add modules

---
+++ js/card.js
@@ -0,0 +1,74 @@
+'use strict';
+
+window.cardSet = (function () {
+  var TYPE_NAMES = {
+    'flat': 'Квартира',
+    'home': 'Дом',
+    'house': 'Дворец',
+    'bungalo': 'Бунгало'
+  };
+
+  var dialogWindow = document.querySelector('.dialog');
+  var dialogClose = dialogWindow.querySelector('.dialog__close');
+  var pinActive;
+
+  // -----> Закрытие окна с объявлением по клику или Enter <-----
+  var onCloseDialog = function (evt) {
+    if (window.dataSet.isEnterPressed(evt) || window.dataSet.isClicked(evt)) {
+      pinActive = document.querySelector('.pin--active');
+      if (pinActive) {
+        pinActive.classList.remove('pin--active');
+      }
+      dialogWindow.style.display = 'none';
+      document.removeEventListener('keydown', onCloseDialogEsc);
+    }
+  };
+
+  // -----> Закрытие окна с объявлением по Esc <-----
+  var onCloseDialogEsc = function (evt) {
+    if (window.dataSet.isEscapePressed(evt)) {
+      pinActive = document.querySelector('.pin--active');
+      if (pinActive) {
+        pinActive.classList.remove('pin--active');
+        pinActive = '';
+      }
+      dialogWindow.style.display = 'none';
+    }
+  };
+
+  // -----> Добавление данных о квартире в DOM <-----
+  return function (advertItem) {
+    var lodgeTemplate = document.querySelector('#lodge-template').content;
+    var lodgeItem = lodgeTemplate.cloneNode(true);
+    var lodgeTitle = lodgeItem.querySelector('.lodge__title');
+    var lodgeAddress = lodgeItem.querySelector('.lodge__address');
+    var lodgePrice = lodgeItem.querySelector('.lodge__price');
+    var lodgeType = lodgeItem.querySelector('.lodge__type');
+    var lodgeRooms = lodgeItem.querySelector('.lodge__rooms-and-guests');
+    var lodgeCheckin = lodgeItem.querySelector('.lodge__checkin-time');
+    var dialog = document.querySelector('.dialog');
+    var dialogPanel = document.querySelector('.dialog__panel');
+
+    lodgeTitle.textContent = advertItem.offer.title;
+    lodgeAddress.textContent = advertItem.offer.address;
+    lodgePrice.innerHTML = advertItem.offer.price + ' ' + '&#8381;/ночь';
+    lodgeType.textContent = TYPE_NAMES[advertItem.offer.type];
+    lodgeRooms.textContent = 'Для ' + advertItem.offer.guests + ' гостей в ' + advertItem.offer.rooms + ' комнатах';
+    lodgeCheckin.textContent = 'Заезд после ' + advertItem.offer.checkin + ', выезд до ' + advertItem.offer.checkout;
+
+    for (var i = 0; i < advertItem.offer.features.length; i++) {
+      var span = document.createElement('span');
+      span.className = 'feature__image feature__image--' + advertItem.offer.features[i];
+      lodgeItem.querySelector('.lodge__features').appendChild(span);
+    }
+
+    lodgeItem.querySelector('.lodge__description').textContent = advertItem.offer.description;
+    document.querySelector('.dialog__title img').src = advertItem.author.avatar;
+
+    dialog.replaceChild(lodgeItem, dialogPanel);
+    dialogWindow.style.display = 'block';
+    document.addEventListener('keydown', onCloseDialogEsc);
+    dialogClose.addEventListener('click', onCloseDialog);
+    dialogClose.addEventListener('keydown', onCloseDialog);
+  };
+})();
---
+++ js/pin.js
@@ -0,0 +1,79 @@
+'use strict';
+
+window.pinSet = (function () {
+  var pinsMap = document.querySelector('.tokyo__pin-map');
+  var pinActive = document.querySelector('.pin--active');
+  var dialogWindow = document.querySelector('.dialog');
+  var advertsList = window.dataSet.createAdv();
+
+  // -----> Создание пина <-----
+  var createPin = function (item, state) {
+    var pin = document.createElement('div');
+    var img = document.createElement('img');
+    if (state === 0) {
+      pin.className = 'pin pin--active';
+      pinActive = pin;
+    } else {
+      pin.className = 'pin';
+    }
+    pin.style.left = item.location.x - pin.offsetWidth / 2 + 'px';
+    pin.style.top = item.location.y - pin.offsetHeight + 'px';
+    img.className = 'rounded';
+    img.width = 40;
+    img.height = 40;
+    img.src = item.author.avatar;
+    pin.appendChild(img);
+    pin.setAttribute('tabindex', 0);
+
+    return pin;
+  };
+
+  // -----> Поиск номера объявления по данным аватара <-----
+  var searchAdvert = function (current) {
+    for (var i = 0; i < advertsList.length; i++) {
+      if (advertsList[i].author.avatar === current) {
+        break;
+      }
+    }
+    return i;
+  };
+
+  // -----> Показать объявление, если на пин кликнули или нажали Enter <-----
+  var onShowDialog = function (evt) {
+    if (window.dataSet.isEnterPressed(evt) || window.dataSet.isClicked(evt)) {
+      var currentPin = '';
+      var currentSrc = '';
+      var currentTarget = evt.target;
+      if (evt.target.className === 'rounded') {
+        currentPin = currentTarget.offsetParent;
+        currentSrc = currentTarget.getAttribute('src');
+      } else if (currentTarget.className === 'pin') {
+        currentPin = currentTarget;
+        currentSrc = currentTarget.children[0].getAttribute('src');
+      }
+      // -----> Если у пина уже есть pin--active, то его надо убрать <-----
+      if (pinActive) {
+        pinActive.classList.remove('pin--active');
+      }
+      currentPin.classList.add('pin--active');
+      pinActive = currentPin;
+      // -----> Создание окна диалога для выбранного пина <-----
+      var pinNumber = searchAdvert(currentSrc, advertsList);
+      window.cardSet(advertsList[pinNumber]);
+    }
+  };
+
+  // -----> Добавление пинов в DOM <-----
+  return function () {
+    // var pinMap = document.querySelector('.tokyo__pin-map');
+    var fragment = document.createDocumentFragment();
+    for (var i = 0; i < advertsList.length; i++) {
+      fragment.appendChild(createPin(advertsList[i]));
+    }
+    pinsMap.appendChild(fragment);
+    dialogWindow.style.display = 'none';
+    pinsMap.addEventListener('click', onShowDialog);
+    pinsMap.addEventListener('keydown', onShowDialog);
+  };
+
+})();
